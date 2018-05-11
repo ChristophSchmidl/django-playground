72a58a9ea38469cab154489b3be38cabbbf89a19
Christoph Schmidl <c.schmidl@elearning-manufaktur.com>
2018-05-11 11:31:23 +0200

    Added a custom user profile model, a manager, and the migration for our database

--- build-your-own-backend-rest-api-using-django-rest-framework/src/profiles_project/profiles_project/settings.py
+++ build-your-own-backend-rest-api-using-django-rest-framework/src/profiles_project/profiles_project/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Tell Django to use our custom user model
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
