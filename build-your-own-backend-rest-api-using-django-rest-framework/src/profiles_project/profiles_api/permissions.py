6962b3abfcdfacaf961a858ea0fec9c46c108b1e
Christoph Schmidl <c.schmidl@elearning-manufaktur.com>
2018-05-11 19:36:38 +0200

    Added permissions and authentication to our UserProfileViewSet.

---
+++ build-your-own-backend-rest-api-using-django-rest-framework/src/profiles_project/profiles_api/permissions.py
@@ -0,0 +1,26 @@
+from rest_framework import permissions
+
+
+class UpdateOwnProfile(permissions.BasePermission):
+    """
+    Allows users to edit their own profile.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        """
+        Check user is trying to edit their own profile.
+        :param request:
+        :param view:
+        :param obj:
+        :return:
+        """
+
+        # if the user is just looking at a profile without
+        # changing it then allow the action
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        # is the object's id (profile id) which the user wants to change
+        # the same as the user's id?
+        return obj.id == request.user.id
+
