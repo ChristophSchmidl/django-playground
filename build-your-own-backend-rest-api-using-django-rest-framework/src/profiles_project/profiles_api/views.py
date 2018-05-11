171511959c7dd3b35426304ab87a7309dcd70176
Christoph Schmidl <c.schmidl@elearning-manufaktur.com>
2018-05-11 13:00:56 +0200

    Added POST, PUT, PATCH and DELETE functions to our APIView.

--- build-your-own-backend-rest-api-using-django-rest-framework/src/profiles_project/profiles_api/views.py
+++ build-your-own-backend-rest-api-using-django-rest-framework/src/profiles_project/profiles_api/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+
+from . import serializers
 
 # Create your views here.
 
@@ -9,6 +12,8 @@ class HelloApiView(APIView):
     """
     Test API View.
     """
+
+    serializer_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """
@@ -27,3 +32,57 @@ class HelloApiView(APIView):
 
         # Takes a dictionary
         return Response({'message': 'Hello!', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """
+        Create a hello message with our name.
+        :param request:
+        :return:
+        """
+
+        serializer = serializers.HelloSerializer(data=request.data)
+
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'Hello {0}'.format(name)
+            return Response({'message': message})
+
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+    def put(self, request, pk=None):
+        """
+        Handles updating an object.
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'put'})
+
+
+    def patch(self, request, pk=None):
+        """
+        Patch request, only updates fields provided in the request.
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'patch'})
+
+
+    def delete(self, request, pk=None):
+        """
+        Deletes an object.
+        :param request:
+        :param pk:
+        :return:
+        """
+
+        return Response({'method': 'delete'})
+
